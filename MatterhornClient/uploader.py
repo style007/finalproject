9219dbbb67a9c865363be21a2c5d17ddba4de5fe
Matan <mlachmish@gmail.com>
2015-02-28 18:49:19 +0200

    Uploading Matterhorn python uploader

---
+++ MatterhornClient/uploader.py
@@ -0,0 +1,119 @@
+import json
+import sys
+import os
+import datetime
+import threading
+import requests
+import time
+
+#File containing details of matterhorn server
+CONST_CONF_FILE = 'uploader.conf'
+
+#Chunk size for splitting the video file
+CONST_CHUNK_SIZE = 2072576
+
+def main(argv):
+    # Usage check
+    if 2 != len(argv):
+        print "Usage: %s [movie file]" % argv[0]
+        return -1
+
+    print 'Welcome to TAU Matterhorn lecture uploader'
+
+    confData = json.loads(open(CONST_CONF_FILE).read())
+    matterhornUser = confData["USER"]
+    matterhornPassword = confData["PASSWORD"]
+    matterhornURL = confData["MATTERHORNURL"]
+
+    movieFile = argv[1];
+    movie = open(movieFile, 'rb')
+
+    #Check credentials
+    session = requests.session()
+    login_data = dict(j_username=matterhornUser, j_password=matterhornPassword, submit="Login")
+    session.post(matterhornURL + '/j_spring_security_check', data=login_data)
+    response = session.get(matterhornURL + '/info/me.json')
+    roles = json.loads(response.content)["roles"]
+    if "ROLE_ADMIN" not in roles:
+        print "You must have admin role in order to upload a file"
+        return -1
+
+    lectureTitle = raw_input("Please enter lecture title: ")
+
+    print "Creating a media package"
+    response = session.get(matterhornURL + '/ingest/createMediaPackage')
+    mediaPackage = response.content
+
+    print "Add DC Catalog"
+    params = dict(flavor='dublincore/episode', mediaPackage=mediaPackage,
+                  dublinCore=createDublinCore(lectureTitle, movie.name))
+    response = session.post(matterhornURL + '/ingest/addDCCatalog', data=params)
+    mediaPackage = response.content
+
+    print "Create new job"
+    params = dict(filename=movieFile, filesize='194296984', chunksize='2072576', flavor='presentation/source',
+                  mediapackage=mediaPackage)
+    response = session.post(matterhornURL + '/upload/newjob', data=params)
+    jobID = response.content
+    print "New job was created with id: " + jobID
+
+    print "Upload movie"
+    uploaded = 0
+    chunkNumber = 0
+    movieSize = os.path.getsize(movieFile)
+    numberOfChunks = movieSize / CONST_CHUNK_SIZE
+    print 'Uploading ' + str(movieSize) + 'B in ' + str(numberOfChunks) + ' chunks, this may take a while...'
+    while (uploaded < movieSize):
+        printProgress(int(chunkNumber / float(numberOfChunks) * 100))
+
+        payload = {'chunknumber': chunkNumber, 'jobID': jobID}
+        chunk = movie.read(CONST_CHUNK_SIZE)
+        files = {'filedata': ('blob', chunk), 'type': 'application/octet-stream'}
+        response = session.post(matterhornURL + '/upload/job/' + jobID, files=files, data=payload)
+        # processThread = threading.Thread(target=session.post, args=(matterhornURL + '/upload/job/' + jobID, files, payload,))
+        # processThread.start()
+        # time.sleep(5)
+        uploaded += CONST_CHUNK_SIZE
+        chunkNumber += 1
+
+    print "Job status"
+    while True:
+        response = session.get(matterhornURL + '/upload/job/' + jobID + ".json")
+        jsonResponse = json.loads(response.content)
+        state = jsonResponse["uploadjob"]["state"]
+        print "Job state is: " + state
+        if state == "COMPLETE":
+            fileUri = jsonResponse["uploadjob"]["payload"]["url"]
+            print "File uri is: " + fileUri
+            break
+
+    print "Adding track"
+    params = dict(mediapackage=mediaPackage, trackUri=fileUri, flavor='presentation/source')
+    response = session.post(matterhornURL + '/mediapackage/addTrack', data=params)
+    mediaPackage = response.content
+
+    print "Ingest"
+    params = dict(archiveOp='true', distribution='Matterhorn Media Module', mediaPackage=mediaPackage)
+    response = session.post(matterhornURL + '/ingest/ingest/full', data=params)
+    workflow = response.content
+    print "A new workflow has been created. Success"
+
+
+def createDublinCore(lectureTitle, fileName):
+    recordTime = os.path.getctime(fileName)
+    recordDate = datetime.datetime.fromtimestamp(recordTime).strftime('%Y-%m-%d')
+
+    ts = time.time()
+    createTime = datetime.datetime.fromtimestamp(ts).strftime('%Y-%m-%dT%H:%M:%SZ')
+
+    dublinCore = '<dublincore xmlns="http://www.opencastproject.org/xsd/1.0/dublincore/" xmlns:dcterms="http://purl.org/dc/terms/"><dcterms:title>' + lectureTitle + '</dcterms:title><dcterms:creator></dcterms:creator><dcterms:isPartOf></dcterms:isPartOf><dcterms:license>Creative Commons 3.0: Attribution-NonCommercial-NoDerivs</dcterms:license><dcterms:license></dcterms:license><dcterms:recordDate>' + recordDate + '</dcterms:recordDate><dcterms:contributor></dcterms:contributor><dcterms:subject></dcterms:subject><dcterms:language></dcterms:language><dcterms:description></dcterms:description><dcterms:rights></dcterms:rights><dcterms:created>' + createTime + '</dcterms:created></dublincore>'
+    return dublinCore
+
+
+def printProgress(percentage):
+    sys.stdout.write("\r%d%%" % percentage)
+    sys.stdout.flush()
+
+
+if __name__ == "__main__":
+    main(sys.argv)
